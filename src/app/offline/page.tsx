3a826b5b1e89fb4c3037fc2063de034e5a699535
Zubair Hassan <mzubairhassan18@gmail.com>
2026-07-15 17:34:51 +0500

    Fix offline page: add 'use client' directive for onClick handler

--- src/app/offline/page.tsx
+++ src/app/offline/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 export default function OfflinePage() {
   return (
     <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", minHeight: "100vh", padding: "2rem", textAlign: "center" }}>
